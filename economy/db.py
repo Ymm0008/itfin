e940986cf58b30aa951eeea1a0356217ce2ccc04
lvlei <123456@163.com>
2017-12-25 22:09:33 +0800

    zy

--- economy/db.py
+++ economy/db.py
@@ -63,31 +63,31 @@ def get_project(table,field):
 	return data
 
 
-def platform_detail(table1,table2,id,field):
+def platform_detail(table1,table2,table3,id,field):
 	conn = mysql.connect(host="219.224.134.214",user="root",password="",db="itfin",charset='utf8')
 	conn.autocommit(True)
 	cur = conn.cursor()
-	sql = "select * from %s as el inner join %s as pd on el.id=pd.entity_id where el.id=%d and pd.date='2017-11-27'" % (table1,table2,id)
+	sql = "select * from %s as el inner join %s as pd on el.id=pd.entity_id inner join %s as gs on el.id=gs.entity_id where el.id=%d and pd.date='2017-11-27'" % (table1,table2,table3,id)
 	cur.execute(sql)
 	res = cur.fetchall()
 	data = [{k:row[i] for i,k in enumerate(field)} for row in res]
 	return data
 
-def company_detail(table1,table2,id,field):
+def company_detail(table1,table2,table3,id,field):
 	conn = mysql.connect(host="219.224.134.214",user="root",password="",db="itfin",charset='utf8')
 	conn.autocommit(True)
 	cur = conn.cursor()
-	sql = "select * from %s as el inner join %s as cd on el.id=cd.entity_id where el.id=%d and cd.date='2017-11-27'" % (table1,table2,id)
+	sql = "select * from %s as el inner join %s as cd on el.id=cd.entity_id inner join %s as gs on el.id=gs.entity_id where el.id=%d and cd.date='2017-11-27'" % (table1,table2,table3,id)
 	cur.execute(sql)
 	res = cur.fetchall()
 	data = [{k:row[i] for i,k in enumerate(field)} for row in res]
 	return data
 
-def project_detail(table1,table2,id,field):
+def project_detail(table1,table2,table3,id,field):
 	conn = mysql.connect(host="219.224.134.214",user="root",password="",db="itfin",charset='utf8')
 	conn.autocommit(True)
 	cur = conn.cursor()
-	sql = "select * from %s as el inner join %s as p on el.id=p.entity_id where el.id=%d and p.date='2017-11-27'" % (table1,table2,id)
+	sql = "select * from %s as el inner join %s as p on el.id=p.entity_id inner join %s as gs on el.id=gs.entity_id where el.id=%d and p.date='2017-11-27'" % (table1,table2,table3,id)
 	cur.execute(sql)
 	res = cur.fetchall()
 	data = [{k:row[i] for i,k in enumerate(field)} for row in res]
