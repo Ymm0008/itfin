b7fa369e608582e788f73c92c183b4db513167ed
linhaobuaa <1257819385@qq.com>
2018-01-11 09:44:20 +0800

    zyx

--- economy/detection/views.py
+++ economy/detection/views.py
@@ -18,25 +18,25 @@ def detect():
 @detection.route('/detectData/',methods=['POST','GET'])
 def detect_data():
 	date = request.args.get('date','')
-	result = getDetectData(date,TABLE_ENTITY_LIST,TABLE_PLAT_DETAIL,TABLE_COMPANY_DETAIL,TABLE_PROJECT_DETAIL,TABLE_GONGSHANG,field)
+	result = getDetectData(date,TABLE_ENTITY_LIST,TABLE_MONITOR,TABLE_GONGSHANG,field)
 	return json.dumps(result,ensure_ascii=False)
 
 @detection.route('/detectRank/')
 def detect_rank():
 	date = request.args.get('date','')
-	result = getDetectRank(TABLE_PLAT_DETAIL,TABLE_COMPANY_DETAIL,TABLE_PROJECT_DETAIL,date,rank_field)
+	result = getDetectRank(TABLE_MONITOR,date,rank_field)
 	result.sort(key=lambda x:x['count'],reverse=True)
 
-	return json.dumps(result[0:30],ensure_ascii=False)
+	return json.dumps(result[0:20],ensure_ascii=False)
 
 @detection.route('/detectDistribute/')
 def detect_distribute():
 	date = request.args.get('date','')
-	result = getDetectDistribute(date,TABLE_PLAT_DETAIL,TABLE_COMPANY_DETAIL,TABLE_PROJECT_DETAIL,TABLE_GONGSHANG,dis_field)
+	result = getDetectDistribute(date,TABLE_MONITOR,TABLE_GONGSHANG,dis_field)
 	result.sort(key=lambda x:x['sum'],reverse=True)
 	return json.dumps(result,ensure_ascii=False)
 
 @detection.route('/warnCount/')
 def warn_count():
-	result = getWarnCount(TABLE_PLAT_DETAIL,TABLE_COMPANY_DETAIL,TABLE_PROJECT_DETAIL)
+	result = getWarnCount(TABLE_MONITOR)
 	return json.dumps(result,ensure_ascii=False)
